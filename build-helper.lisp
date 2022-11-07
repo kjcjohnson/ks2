13022a10c1e90e03d3d208524bbb60c62e9be6d1
Keith Johnson <kjcjohnson@ymail.com>
2022-11-06 19:24:00 -0600

    Modularize the build helper

--- build-helper.lisp
+++ build-helper.lisp
@@ -1,8 +1,16 @@
 ;;;;
 ;;;; Bootstrap-helper.lisp - bootstraps the TDP systems
 ;;;;
-(proclaim '(optimize (speed 3) (debug 0)))
-(asdf:oos 'asdf:load-op "com.kjcjohnson.tdp/test" :force t)
-(setf com.kjcjohnson.synthkit.ast:*execution-counter* 0)
-(trivial-garbage:gc :full t)
-(asdf:oos 'asdf:image-op "com.kjcjohnson.ks2.runner/helper")
+(require 'asdf)
+
+(defun build-runner ()
+  "Builds the runner image"
+  (proclaim '(optimize (speed 3) (debug 0)))
+  (asdf:oos 'asdf:load-op "com.kjcjohnson.tdp/test" :force t)
+  (setf com.kjcjohnson.synthkit.ast:*execution-counter* 0)
+  (trivial-garbage:gc :full t)
+  (asdf:oos 'asdf:image-op "com.kjcjohnson.ks2.runner/helper"))
+
+(defun build-app ()
+  "Builds the controller app"
+  (asdf:oos 'asdf:program-op "com.kjcjohnson.ks2.runner"))
