040920f7ada11abd341ace3e3243976cfc28fa1a
Keith Johnson <kjcjohnson@ymail.com>
2023-06-25 20:19:19 -0500

    Get another verbose reference

--- src/cli/hooks.lisp
+++ src/cli/hooks.lisp
@@ -5,13 +5,13 @@
 
 (defun stop-logger ()
   "Stops the global logger"
-  (v:stop verbose:*global-controller*)
+  (v:stop v:*global-controller*)
   ;; Apparently the controller won't stop unless it gets another message
   (v:trace :ks2.cli "Stopping global logging controller..."))
 
 (defun start-logger ()
   "Starts the global logger"
-  (v:start verbose:*global-controller*)
+  (v:start v:*global-controller*)
   (v:trace :ks2.cli "Starting global logging controller..."))
 
 (uiop:register-image-dump-hook #'stop-logger nil)
