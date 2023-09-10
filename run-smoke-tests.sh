a4cad43d92e3380da5ae7b8d232822c941657ad5
Keith Johnson <kjcjohnson@ymail.com>
2023-09-10 13:08:12 -0500

    Add initial smoke tests

---
+++ run-smoke-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# Runs smoke tests as a quick check that the solver is working
+#
+
+# Find the build directory. Adapted from SBCL's run-sbcl.sh
+this="$0"
+
+build_directory_p(){
+    [ -x "$1"/bin/ks2 ];
+}
+
+# OSX 10.8 readlink doesn't have -f
+while [ -h "$this" ]; do
+    # Stop resolving symlinks when $this lives in a build tree.
+    # (Build trees might consist of symlinks to something that doesn't
+    # follow our repo layout.)
+    if build_directory_p "$(dirname "$this")"; then
+	break
+    fi
+    # [ -h should guarantee that readlink output will be non-null
+    link="$(readlink -n "$this")"
+    # if absolute path
+    if expr "$link" : '^/.*' > /dev/null; then
+        this="$link"
+    else
+        this="$(dirname "$this")"/"$link"
+    fi
+done
+root_dir="$(dirname "$this")"
+test_dir="${root_dir}/t/smoke-tests"
+exe="${root_dir}/bin/ks2"
+
+for test in "$test_dir"/*.sem; do
+    echo " --> $test"
+    $exe solve -s tde "$test"
+    echo " +++ $test" 
+done
